1413c418bdb6ef6e63b945b9713fff7c511b1452
Carter Patton <pattoncarter@yahoo.com>
2023-03-30 11:12:48 -0500

    created basic file structure

---
+++ project3/app/manager/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Customer(){
+    return(
+        <main>
+                <h1>This is the Manager UI</h1>
+        </main>
+    )
+}
