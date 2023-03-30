88b8d9bf7509621abc3cd2aa361335a3b3d44490
Carter Patton <pattoncarter@yahoo.com>
2023-03-30 11:56:05 -0500

    Created Prisma connection to database

---
+++ project3/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head(){
+    return(
+        <>
+        <title> Houston Street Subs PoS</title>
+        <meta content = "width=device-width, intial-scale-1" name = "viewport"/>
+        <meta  name = "description" content="Project3-MU's Upgraded PoS System" />
+        <link rel="icon" href="/favicon.ico"/>
+        </>
+    )
+}
--- project3/app/page.tsx
+++ project3/app/page.tsx
@@ -1,13 +1,12 @@
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
+import Link from "next/link"
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div>
-        <h1> Home </h1>
+    <main>
+      <div className = "py-78 px-78">
+        <Link href = {"/dahsboard"}>Go To Dashboard </Link>
       </div>
     </main>
   )
