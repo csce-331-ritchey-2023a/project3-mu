eafd8aaf5993517bdade977afdc583e9695bab52
Carter Patton <pattoncarter@yahoo.com>
2023-04-04 16:23:34 -0500

    Fixed getName

--- project3/app/api/getName/route.ts
+++ project3/app/api/getName/route.ts
@@ -5,8 +5,7 @@ import {NextResponse} from "next/server"
 //TODO: need to add fucnitonality for parsing through the multiple ints that can be passed
 export async function GET(request: Request, { params } : {params:any}) {
     //fetch menu items with prisma
-    
     const data = await prisma.menu_items_table.findMany({where: {"category":"Sand"}})
-    console.log(data)
+    //console.log(data)
     return NextResponse.json(data)
 }
