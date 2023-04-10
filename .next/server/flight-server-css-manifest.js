f53088b7737e20e1a4455ab5bf48cc06bc99f0b4
Carter Patton <pattoncarter@yahoo.com>
2023-04-10 11:36:45 -0500

    Added more routing capabilities for individual items

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,43 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/customer/Salad/page": [
+      "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/globals.css"
+    ],
+    "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/customer/Soup/page": [
+      "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/globals.css"
+    ],
+    "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/customer/page": [
+      "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/globals.css"
+    ],
+    "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/page": [
+      "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/globals.css"
+    ],
+    "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/customer/Sand/page": [
+      "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/globals.css"
+    ],
+    "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/customer/Salad/[slug]/page": [
+      "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/globals.css"
+    ]
+  },
+  "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/layout.tsx": [
+    "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/globals.css"
+  ],
+  "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/customer/Salad/page.tsx": [
+    "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/globals.css"
+  ],
+  "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/customer/Soup/page.tsx": [
+    "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/globals.css"
+  ],
+  "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/customer/page.tsx": [
+    "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/globals.css"
+  ],
+  "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/page.tsx": [
+    "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/globals.css"
+  ],
+  "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/customer/Sand/page.tsx": [
+    "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/globals.css"
+  ],
+  "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/customer/Salad/[slug]/page.tsx": [
+    "/Users/carterpatton/Desktop/331/project3-mu/project3-mu/app/globals.css"
+  ]
+}
