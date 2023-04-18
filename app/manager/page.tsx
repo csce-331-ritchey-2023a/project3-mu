22fd681930834818e52ed7ebc5a639e567ca961b
SamnBierle <sam.nbierle@gmail.com>
2023-04-18 10:00:26 -0500

    Updated Button Alignment, Text Input Boxes, and Dropdown values to better reflect the Manager GUI Functions

--- app/manager/page.tsx
+++ app/manager/page.tsx
@@ -22,44 +22,39 @@ export default function Manager() {
       </div>
       <div className="center">
         <div className="pair">
-          <div className="textbox">
-            Top 5 Sellers
+
+          <button type="button" className="textbox">
+            Execute Sales Report
+          </button>
+          <div className="Input_Box">
+            <input type="text" placeholder="Enter Starting Date (MM/DD/YYYY)" />
           </div>
+          <div className="Input_Box">
+            <input type="text" placeholder="Enter Ending Date (MM/DD/YYYY)" />
+          </div>
+
+          <button type="button" className="textbox">
+            Execute Excess Report
+          </button>
+          <div className="Input_Box">
+            <input type="text" placeholder="Enter information" />
+          </div>
+
+          <button type="button" className="textbox">
+            Execute Report:
+          </button>
           <div className="dropdown">
             <select>
-              <option value="item1">Today</option>
-              <option value="item2">Yesterday</option>
-              <option value="item3">04/09/2023</option>
-              <option value="item4">04/08/2023</option>
-            </select>
-          </div>
-          <div className="textbox">
-            Most Used Item
-          </div>
-          <div className="dropdown">
-            <select>
-              <option value="item1">Today</option>
-              <option value="item2">Yesterday</option>
-              <option value="item3">04/09/2023</option>
-              <option value="item4">04/08/2023</option>
-            </select>
-          </div>
-          <div className="textbox">
-            Order By
-          </div>
-          <div className="dropdown">
-            <select>
-              <option value="item1">Yesterday</option>
-              <option value="item2">04/09/2023</option>
-              <option value="item3">04/08/2023</option>
-              <option value="item4">04/07/2023</option>
+              <option value="item1">X Report</option>
+              <option value="item2">Z Report</option>
             </select>
           </div>
         </div>
         <div className="whitebox">
-          This is a white text box below the pairings.
+          For Sales and Excess Reports Enter Data in MM/DD/YYYY form
         </div>
       </div>
     </main>
   )
 }
+
