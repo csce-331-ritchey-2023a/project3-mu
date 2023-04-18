9c27732b2812e0fd507e231a363263fa4a91beb6
santiago-fonseca <73078265+santiago-fonseca@users.noreply.github.com>
2023-04-18 17:01:12 -0500

    Add files via upload

--- app/api/getName2/route.ts
+++ app/api/getName2/route.ts
@@ -1,1 +1,14 @@
-ss
+
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+import {NextResponse} from "next/server"
+
+//TODO: need to add fucnitonality for parsing through the multiple ints that can be passed
+export async function GET(request: Request, { params } : {params:any}) {
+    //fetch menu items with prisma
+    
+    //Fetch all Sandwich type items
+    const data = await prisma.menu_items_table.findMany({where: {"category" :"Salad"}}) //if category == Sand
+    console.log("here")
+    return NextResponse.json(data)
+}
