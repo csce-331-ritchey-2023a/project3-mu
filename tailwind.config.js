d49b28f011abe831a8732b40892c683b5ac70421
Carter Patton <pattoncarter@yahoo.com>
2023-04-18 10:58:15 -0500

    Attempting to patch styling in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   content: [
   "./app/**/*.{js,ts,jsx,tsx}",
   "./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",],
+  "./components/**/*.{js,ts,jsx,tsx}",
+  "./layout/**/*.{js,ts,jsx,tsx}",
+  "!./node_modules", //
+  ],
   theme: {
     // screens: {
     //   'sm': {'max': '639px'},
