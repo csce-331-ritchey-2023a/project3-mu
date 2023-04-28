416ebd097d8f2f3989ca0776ef7e137f77980288
SamnBierle <sam.nbierle@gmail.com>
2023-04-28 10:30:58 -0500

    Changed API variable

--- app/Google_Maps/index.js
+++ app/Google_Maps/index.js
@@ -3,7 +3,7 @@ import {GoogleMap, useLoadScript, Marker} from "@react-google-maps/api";
 import 'app/globals.css';
 
 export default function Home() {
-    const {isLoaded} = useLoadScript ({googleMapsApiKey: AIzaSyDVx7wMDvAIuW2La2bcJQZ2EK0sJdnIIts});
+    const {isLoaded} = useLoadScript ({googleMapsApiKey: NEXT_PUBLIC_GOOGLE_MAPS_API_KEY});
 
     // If google maps fails to load, display loading message
     if (!isLoaded) return <div>Loading...</div>;
