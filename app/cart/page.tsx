2e3505ae4f9fe5a11d628cf1d21424eeb2a718b1
Carter Patton <pattoncarter@yahoo.com>
2023-05-01 10:09:08 -0500

    preserve styling on complete page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -43,7 +43,7 @@ const CartPage = () => {
             <Link href="/customer">Order More</Link>  
           </button>
           <button className="mx-auto rounded-lg bg-red-500 hover:bg-gray-500 py-2 px-4 text-white ">
-            <a href="/Complete">Checkout</a>
+            <Link href="/Complete">Checkout</Link>
           </button>         
       </div>
       <button></button>
