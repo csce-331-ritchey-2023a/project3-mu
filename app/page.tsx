26a8d83fb9d85ee3340f64768002694054a3704b
Carter Patton <pattoncarter@yahoo.com>
2023-05-01 08:54:35 -0500

    Stylized select screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,27 @@
 import Link from "next/link"
-import { use } from "react"
 import './globals.css'
-async function getItems(){
-  const res = await fetch(`${process.env.BASE_URL}/api/getItems`, {cache: "no-store"})
-  if(!res.ok){
-    console.log(process.env.BASE_URL)
-  }
-  return await(res).json()
-}
-
 export default function Home() {
   //const data = use(getItems())
   //console.log(data)
   return (
-    <main className="bg-primary">
-      <nav className = "flex justify-center">
-        <Link href = {"/customer"} className="flex justify-left">Go To Customer </Link>
-        <Link href = {"/employee"}>Go To Employee </Link>
-        <Link href = {"/manager"}>Go To Manager </Link>
-        <Link href = {"/Weather"}>Go To Weather </Link>
-      </nav>
-      <div className="flex justify-center">
-        <h1>Welcome to Houston Street Subs!</h1>
-      </div>
-    </main>
+      <main className="bg-primary h-screen flex flex-col justify-center items-center">
+        <nav className="flex justify-center space-x-4 mb-8">
+          <Link href={"/customer"}
+            className="px-4 py-2 bg-secondary text-white rounded-md hover:bg-primary transition-colors duration-300 border border-white">Go To Customer
+          </Link>
+          <Link href={"/employee"}
+            className="px-4 py-2 bg-secondary text-white rounded-md hover:bg-primary transition-colors duration-300 border border-white">Go To Employee
+          </Link>
+          <Link href={"/manager"}
+            className="px-4 py-2 bg-secondary text-white rounded-md hover:bg-primary transition-colors duration-300 border border-white">Go To Manager
+          </Link>
+          <Link href={"/Weather"}
+            className="px-4 py-2 bg-secondary text-white rounded-md hover:bg-primary transition-colors duration-300 border border-white">Go To Weather
+          </Link>
+        </nav>
+        <div className="text-3xl font-bold text-center">
+          <h1>Welcome to Houston Street Subs!</h1>
+        </div>
+      </main>
   )
 }
