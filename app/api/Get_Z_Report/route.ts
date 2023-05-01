e9ccbec369ab793ecd1e55b65f9b7d76b1979bc1
SamnBierle <sam.nbierle@gmail.com>
2023-05-01 08:51:14 -0500

    Added Prisma for Z Report

---
+++ app/api/Get_Z_Report/route.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+import {NextResponse} from "next/server"
+
+export async function GET(request: Request) {
+  try{
+    //fetch items photos with prisma
+    const data = await prisma.sales.findMany({
+      select: {
+        "itemid":true, 
+        "units_sold":true, 
+        "cost_per_unit":true,
+        "revenue_per_unit":true, 
+        "weight_per_unit":true, 
+        "name":true,
+      }
+    })
+    console.log(data);
+    return NextResponse.json(data)
+  }catch(error){
+    console.log('getZReport error:', error);
+    return null
+  }
+}
